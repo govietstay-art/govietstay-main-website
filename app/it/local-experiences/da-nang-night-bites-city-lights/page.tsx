a2d7d1d712ba5d3921aba8b2db2c98a99bfccbad
govietstay-art <govietstay@gmail.com>
2026-09-21 08:15:42 +0700

    Add full Italian Night Bites and City Lights preview route

---
+++ app/it/local-experiences/da-nang-night-bites-city-lights/page.tsx
@@ -0,0 +1,3 @@
+import NightBitesCityLightsPage, {nightMetadata} from '../../../../components/NightBitesCityLightsPage';
+export const metadata = nightMetadata('it');
+export default function Page(){return <NightBitesCityLightsPage locale="it"/>;}
