f3c0d84efb550c272d794b015868e2f8c5c3f354
govietstay-art <govietstay@gmail.com>
2026-09-21 08:34:01 +0700

    Clearly state English guide as default on Italian Night Bites page and link remaining tours

--- app/it/local-experiences/da-nang-night-bites-city-lights/page.tsx
+++ app/it/local-experiences/da-nang-night-bites-city-lights/page.tsx
@@ -1,3 +1,4 @@
 import NightBitesCityLightsPage, {nightMetadata} from '../../../../components/NightBitesCityLightsPage';
-export const metadata = nightMetadata('it');
-export default function Page(){return <NightBitesCityLightsPage locale="it"/>;}
+import LocalExperiencesGuideNotice from '../../../../components/LocalExperiencesGuideNotice';
+export const metadata=nightMetadata('it');
+export default function Page(){return <><LocalExperiencesGuideNotice locale="it"/><NightBitesCityLightsPage locale="it"/><LocalExperiencesGuideNotice locale="it" placement="bottom"/></>;}
