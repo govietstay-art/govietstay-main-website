c0b371c5ebc6478bda884374e7160d422449d8bf
govietstay-art <govietstay@gmail.com>
2026-09-21 08:33:46 +0700

    Show English-standard guide policy and links to new tours on English Night Bites page

--- app/travel/local-experiences/da-nang-night-bites-city-lights/page.tsx
+++ app/travel/local-experiences/da-nang-night-bites-city-lights/page.tsx
@@ -1,3 +1,4 @@
 import NightBitesCityLightsPage, {nightMetadata} from '../../../../components/NightBitesCityLightsPage';
-export const metadata = nightMetadata('en');
-export default function Page(){return <NightBitesCityLightsPage locale="en"/>;}
+import LocalExperiencesGuideNotice from '../../../../components/LocalExperiencesGuideNotice';
+export const metadata=nightMetadata('en');
+export default function Page(){return <><LocalExperiencesGuideNotice locale="en"/><NightBitesCityLightsPage locale="en"/><LocalExperiencesGuideNotice locale="en" placement="bottom"/></>;}
