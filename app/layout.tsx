8920308c42bc35c3a52f8f2673ad58f6d1a2218e
govietstay-art <govietstay@gmail.com>
2026-09-24 12:26:10 +0700

    feat: add Google Tag Manager container GTM-WRPCZ9X3

--- app/layout.tsx
+++ app/layout.tsx
@@ -91,7 +91,24 @@ export default async function RootLayout({
       suppressHydrationWarning
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html:
+              "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WRPCZ9X3');",
+          }}
+        />
+      </head>
       <body className="min-h-full flex flex-col">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WRPCZ9X3"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+            title="Google Tag Manager"
+          />
+        </noscript>
         <HtmlLanguageSync />
         <YandexMetrika />
         <script
